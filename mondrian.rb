9f48797d573c0fa43e4d4e920cc7fd9cca9f34d2
Ryan Davis <ryand-ruby@zenspider.com>
2018-07-01 18:20:00 -0700

    This draws 5 lines w/ random x values properly.
    But, they don't look good, because rand doesn't care about overlaps at
    all.

--- mondrian.rb
+++ mondrian.rb
@@ -5,15 +5,19 @@ require "graphics"
 class Mondrian < Graphics::Simulation
   CLEAR_COLOR = :white
 
+  attr_accessor :xs
+
   def initialize
     super 800, 800
+
+    self.xs = 5.times.map { rand w }
   end
 
   def draw n
     super
 
-    5.times do
-      fast_rect rand(w), 0, 20, h, :black
+    xs.each do |x|
+      fast_rect x, 0, 20, h, :black
     end
   end
 end
