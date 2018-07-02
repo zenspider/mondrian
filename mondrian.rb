8f6811d6aeb5aec08abf84495887e25ef7833387
Ryan Davis <ryand-ruby@zenspider.com>
2018-07-01 18:23:38 -0700

    Add horizontal lines as well.

--- mondrian.rb
+++ mondrian.rb
@@ -5,12 +5,13 @@ require "graphics"
 class Mondrian < Graphics::Simulation
   CLEAR_COLOR = :white
 
-  attr_accessor :xs
+  attr_accessor :xs, :ys
 
   def initialize
     super 800, 800
 
     self.xs = (0..w).step(40).to_a.sample rand(3..7)
+    self.ys = (0..h).step(40).to_a.sample rand(3..7)
   end
 
   def draw n
@@ -18,6 +19,10 @@ class Mondrian < Graphics::Simulation
 
     xs.each do |x|
       fast_rect x, 0, 20, h, :black
+    end
+
+    ys.each do |y|
+      fast_rect 0, y, w, 20, :black
     end
   end
 end
